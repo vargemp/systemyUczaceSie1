9c269a93a325dcde02b2d57a0745d9d7fca3f0e0
Bartosz <barteksz96@gmail.com>
2020-02-26 18:33:34 +0100

    Add project files.

---
+++ systemyUczaceSie1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace systemyUczaceSie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
